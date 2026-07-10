571052a015464de39ad615d9612fd3b1f2b108de
preetx77 <preetsonar77@gmail.com>
2026-07-10 13:35:03 +0530

    config: add settings module with CMS configuration

---
+++ config/settings.py
@@ -0,0 +1,13 @@
+# global project config : 
+
+CMS_BASE_URL = "https://api2.sgx.com/content-api/"
+
+ANNOUNCEMENT_API = "https://api.sgx.com/announcements/v1.1"
+
+CMS_VERSION = "70f75ec90c030bab34d750ee55d74b016f70d4b6"
+
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/137.0.0.0 Safari/537.36"
+)
