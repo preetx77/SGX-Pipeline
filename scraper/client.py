2cf915be694325fb7559befc23ebbe661c82d4d8
preetx77 <preetsonar77@gmail.com>
2026-07-11 15:15:41 +0530

    refactor: fix get_company_announcement to return Announcement objects instead of raw JSON

--- scraper/client.py
+++ scraper/client.py
@@ -6,6 +6,7 @@ Responsible only for communicating with SGX APIs.
 
 import requests
 
+from models.announcement import Announcement
 from scraper.auth import AuthenticationManager
 from config.settings import (
     ANNOUNCEMENT_API,
@@ -72,5 +73,59 @@ class SGXClient:
         return response.json()
 
     def get_company_list(self):
-        """Fetch list of all companies"""
+        # Fetch list of all companies
         return self._get("companylist")
+
+    def get_company_announcement(
+        self,
+        company_name,
+        page_start=0,
+        page_size=100,
+        period_start=None,
+        period_end=None
+    ):
+
+        params = {
+            "periodstart" : period_start,
+            "periodend" : period_end,
+            "value" : company_name,
+            "exactsearch": "true",
+            "pagestart" : page_start,
+            "pagesize" : page_size
+        }
+
+        response = self._get(
+            "company",
+            params=params
+        )
+
+        announcements = []
+
+        for item in response.get("data", []):
+            announcements.append(
+                self._json_to_announcement(item)
+            )
+
+        return announcements
+
+# Function would be the heart of client : every sgx json will pass through here exactly once
+# Convert raw SGX API JSON into an Announcement object.
+    
+    def _json_to_announcement(self, item: dict) -> Announcement:
+
+        issuer = item.get("issuers", [{}])[0]  # Get first issuer from list
+        return Announcement(
+            announcement_id=item.get("id"),
+            ref_id=item.get("ref_id"),
+            company_name=item.get("security_name"),
+            stock_code=issuer.get("stock_code"),
+            isin_code=issuer.get("isin_code"),
+            title=item.get("title"),
+            category=item.get("category_name"),
+            category_code=item.get("cat"),
+            subcategory_code=item.get("sub"),
+            announcement_url=item.get("url"),
+            submission_date=item.get("submission_date"),
+            submission_timestamp=item.get("submission_date_time"),
+            submitted_by=item.get("submitted_by"),
+        )
