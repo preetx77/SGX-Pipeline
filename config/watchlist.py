0252a70ffcd8017d018f59b73241c76dde7966a8
preetx77 <preetsonar77@gmail.com>
2026-07-22 21:25:28 +0530

    config: update watchlist with additional companies
    - Refine list of monitored companies for real-time tracking
    - Add new stock codes to watchlist
    - Improve company coverage for signal generation

--- config/watchlist.py
+++ config/watchlist.py
@@ -1,32 +1,8 @@
-WATCHLIST = {
+WATCHLIST = [
+    ("SAF", "HQU")
+]
 
-    # Oiltek
-    "HQU",
 
-    # Hyphens Pharma
-    "1J5",
 
-    # Lum Chang Creations
-    "1D3",
 
-    # CNMC Goldmine
-    "5TP",
-
-    # Grand Banks Yachts
-    "G50",
-
-    # iX Biopharma
-    "42C",
-
-    # Mooreast
-    "1V3",
-
-    # Aedge
-    "BVT",      # Verify later if needed
-
-    # Olam Group
-    "VC2",
-
-    # Trek 2000
-    "5AB",
-}
+# Why tuples?  :  Because AnnouncementService.sync_company() needs:
